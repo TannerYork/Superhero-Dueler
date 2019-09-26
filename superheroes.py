dce0dfbfc6a7fbe317d4d788b821a0841b1c5273
Tanner York <tannerwyork@gmail.com>
2019-09-25 17:53:05 -0700

    [Add] Create the areana build team methods

---
+++ superheroes.py
@@ -0,0 +1,299 @@
+import random
+import math
+from functools import reduce
+
+
+class Ability():
+    def __init__(self, name, attack_strength):
+        '''
+        Initiate the abilities class with its name and attack strength
+            Args:
+                name (string): a single word discriptor of the ability
+                attack_strength (int): the value of the abilities strength
+            Returns:
+                ability object: a new object
+        '''
+        self.name = name
+        self.max_damage = attack_strength
+
+    def attack(self):
+        ''' A function that returns random int between 0 and the initilized max_damage property'''
+        return random.randint(0, self.max_damage)
+
+
+class Weapon(Ability):
+    def __init__(self, name, attack_strength):
+        '''
+        Initiate the weapons subclass with name and attack_strength
+            Args:
+                name (string): a single word descriptor of the weapon
+                attack_strength (int): value for the weapons may power
+            Returns:
+                weapon (object): new weapon object
+        '''
+        super().__init__(name, attack_strength)
+
+    def attack(self):
+        '''A function that returns a randome int between 0 and half the iniitlaized max_damage property'''
+        return random.randint(0, math.floor(self.max_damage / 2))
+
+
+class Armor():
+    def __init__(self, name, blocking_strength):
+        '''
+        Initiate the armor class with name and blocking strength
+            Args:
+                name (string): a detailed one word discriptor of the armor
+                blocking_strength (int): the max value that the armor can block
+            Returns:
+                armor (object): a new armor object
+        '''
+        self.name = name
+        self.max_block = blocking_strength
+
+    def block(self):
+        '''A function that returns a random int between 0 and the initalized max_block property'''
+        return random.randint(0, self.max_block)
+
+
+class Hero():
+    # TODO: Make a way for hero abilities to run out or be lost, and watch for heroes still having abilities
+    def __init__(self, name, starting_health=100, abilities=[], armors=[]):
+        '''
+        Initiate the hero class with name, starting health, kills, and deaths
+            Args:
+                name (string): a one word discription of the hero
+                starting_health (int): a value for the heroes inital health with a default of 100
+            Returns:
+                hero (object): a new hero object
+        '''
+        self.name = name
+        self.abilities = abilities
+        self.armors = armors
+        self.starting_health = starting_health
+        self.current_health = starting_health
+        self.kills = 0
+        self.deaths = 0
+
+    def add_kill(self, num_kills):
+        '''A function for adding a kill to the heroes kill count'''
+        self.kills += num_kills
+
+    def add_death(self, num_deaths):
+        '''A function for adding a death the the heroes death count'''
+        self.deaths += num_deaths
+
+    def add_ability(self, new_ability):
+        '''A setter function that adds a new ability to the hero'''
+        self.abilities.append(new_ability)
+
+    def add_armor(self, new_armor):
+        '''A setter function that adds a new armor object to the hero'''
+        self.armors.append(new_armor)
+
+    def add_weapon(self, weapon):
+        '''A function for adding a new weapon object to the hero'''
+        self.abilities.append(weapon)
+
+    def attack(self):
+        '''A getter function that creates a list of attack values by looping through the heroes abilities and then returns the sum of the list'''
+        return sum(ability.attack() for ability in self.abilities)
+
+    def defend(self, damage_amount):
+        '''
+        A getter function that creates a list of block values by looping through the heroes armors and then returns the sum of the list
+            Args:
+                damage_amount (int): the amount of damage given to the hero
+            Returns:
+                damage_taken (int): the damage_taken subtracted by the heroes total block
+        '''
+        total_block_value = sum(armor.block() for armor in self.armors)
+        damage_taken = damage_amount - total_block_value
+
+        if damage_taken < 0:
+            return 0
+        elif damage_taken > self.starting_health:
+            return self.starting_health
+        else:
+            return damage_taken
+
+    def take_damage(self, damage):
+        '''
+        A setter function that updates the users current_health to reflect the damage minus the defence
+            Args:
+                damage (value): the value of the damage given to the hero
+            Updates:
+                current_health (int): sets the curret health equal to itself and the damage taken from defending
+        '''
+        self.current_health -= self.defend(damage)
+
+    def is_alive(self):
+        '''A getter function that returns true of false depending on whether or not the hero is alive'''
+        if self.current_health <= 0:
+            return False
+        else:
+            return True
+
+    def fight(self, opponent):
+        '''
+        A function that takes in a hero to fight and pits them agians the current hero untill one dies or no ailities are left
+            Args:
+                opponent (Hero): a hero object
+            Returns:
+                outcome (string): either which hero won or that the fight endded in a draw
+        '''
+        while self.is_alive() and opponent.is_alive():
+            hero_attack = self.attack()
+            opponent_attack = opponent.attack()
+            self.take_damage(opponent_attack)
+            opponent.take_damage(hero_attack)
+        if self.is_alive() == False and opponent.is_alive == False:
+            print('Draw! Both heroes died from their injuries!')
+            self.add_kill(1)
+            self.add_death(1)
+        elif self.is_alive() == False:
+            print(f'{opponent.name} won!')
+            self.add_death(1)
+        elif opponent.is_alive() == False:
+            print(f'{self.name} won!')
+            self.add_kill(1)
+
+
+class Team():
+    def __init__(self, name):
+        '''
+        Initialize the team class with name and heroes properties
+            Args:
+                name (string): a single word descriptor of the team
+            Returns:
+                team (object): a new team object
+        '''
+        self.name = name
+        self.heroes = []
+
+    def add_hero(self, hero):
+        '''A function for adding a new hero to the team'''
+        self.heroes.append(hero)
+
+    def remove_hero(self, hero_name):
+        '''A function for removing a hero from the team'''
+        for index, hero in enumerate(self.heroes):
+            if hero.name == hero_name:
+                del self.heroes[index]
+
+    def view_all_heroes(self):
+        '''A function for seeing all the heroes on the team'''
+        heroes = ' '.join(self.heroes)
+        print(heroes)
+
+    def attack(self, other_team):
+        '''A function for battling the team against another'''
+        team_alive = filter(hero.is_alive() for hero in self.heroes)
+        opponents_alive = filter(hero.is_alive() for hero in other_team.heroes)
+        while len(team_alive) > 0 and len(opponents_alive) > 0:
+            team_hero = random.random(team_alive)
+            opponent_hero = random.random(opponents_alive)
+            team_hero.fight(opponent_hero)
+            team_alive = filter(hero.is_alive() for hero in self.heroes)
+            opponents_alive = filter(hero.is_alive()
+                                     for hero in other_team.heroes)
+
+    def revive_heroes(self, health=100):
+        '''A function for resetting each heroes health to its starting_health'''
+        for hero in self.heroes:
+            hero.current_health == hero.starting_health
+
+    def stats(self):
+        '''A function for printing the teams stats'''
+        for hero in self.heroes:
+            kill_death_ratio = hero.kills / hero.deaths
+            print(f'{hero.name}: {kill_death_ratio}')
+
+
+class Arena():
+    def __init__(self):
+        '''Initialate the arena class with a team one and team two properties'''
+        self.team_one = None
+        self.team_two = None
+
+    def create(self, type_prompt, type_reference):
+        '''
+        A geneator function for prompting the user for values to create a specific object
+            Args:
+                type_prompt (sting): type to enter into the input promts
+                type_reference (class): class refrence for the type to create
+            Returns:
+                object: a new type_reference object
+        '''
+        print(f'To create an {type_prompt} enter the following:')
+        name = input('Name: ')
+        while name == '' or name == ' ':
+            name = input('Invaled Input, try agian: ')
+        strength = input('Strength: ')
+        while not strength.isnumeric():
+            strength = input('Invaled Input, try agian: ')
+        return type_reference(name, int(strength))
+
+    def create_hero(self):
+        '''A gernator function for prompting the suer for values to create a Hero instance'''
+        print('To create a hero  follow the steps below:')
+        name = input('Name: ')
+        while name == '' or name == ' ':
+            name = input('Invaled Input, try agian: ')
+        health = input(f'How much health do you want {name} to have? ')
+        while not health.isnumeric():
+            health = input('Invaled input, try again: ')
+        numb_armors = input(f'How much armor do you want {name} to have? ')
+        while not numb_armors.isnumeric():
+            numb_armors = input('Invaled input, try again: ')
+        armors = [self.create('Armor', Armor)
+                  for index in range(numb_armors)]
+        numb_abilities = input(
+            f'How may abilities do you want {name} to have? ')
+        while not numb_abilities.isnumeric():
+            numb_abilities = input('Invaled input, try again: ')
+        abilities = [self.create('Ability', Ability)
+                     for index in range(numb_abilities)]
+        numb_weapons = input(
+            f'How may weapons do you want {name} to have? ')
+        while not numb_weapons.isnumeric():
+            numb_weapons = input('Invaled input, try again: ')
+        abilities += [self.create('Weapon', Weapon)
+                      for index in range(numb_weapons)]
+        return Hero(name, int(health), abilities, armors)
+
+    def build_team_one(self):
+        '''A gernator function for prompting the user for values to create team one for the arena'''
+        print('Create the first team by following the steps below: ')
+        numb_heros = input('How many heros are on this team? ')
+        while not numb_heros.isnumeric():
+            numb_heros = input('Invaled input, try agian: ')
+        self.team_one = [self.create_hero() for index in range(numb_heros)]
+
+    def build_team_two(self):
+        '''A gernator function for prompting the user for values to create team two for the arena'''
+        print('Create the second team by following the steps below: ')
+        numb_heros = input('How many heros are on this team? ')
+        while not numb_heros.isnumeric():
+            numb_heros = input('Invaled input, try agian: ')
+        self.team_two = [self.create_hero() for index in range(numb_heros)]
+
+
+if __name__ == '__main__':
+    # Test the superhero classes by running `python3 superheroes.py`
+    ability = Ability('Kick', 15)
+    another_ability = Ability('Punch', 10)
+    armor = Armor('Shirt', 5)
+    another_armor = Armor('Mouth Guard', 8)
+
+    Bob = Hero('Bob Johson')
+    Bob.add_ability(ability)
+    Bob.add_ability(another_ability)
+    Bob.add_armor(armor)
+    Bob.add_armor(another_armor)
+
+    Jan_The_Man = Hero('Jan the Man')
+    Jan_The_Man.add_ability(ability)
+    Jan_The_Man.add_ability(another_ability)
+    Jan_The_Man.add_armor(armor)
+    Jan_The_Man.add_armor(another_armor)
